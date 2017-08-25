6df983e411b78913f62a0dbed82dbe7ccfe54435
WeiSerena <871876694@qq.com>
2017-08-25 14:16:52 +0800

    提交记录

---
+++ Project/TestView.h
@@ -0,0 +1,13 @@
+//
+//  TestView.h
+//  BreakPointHttp
+//
+//  Created by weiwei-zhang on 17/8/25.
+//  Copyright © 2017年 weiwei-zhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestView : UIView
+
+@end
---
+++ Project/TestView.m
@@ -0,0 +1,21 @@
+//
+//  TestView.m
+//  BreakPointHttp
+//
+//  Created by weiwei-zhang on 17/8/25.
+//  Copyright © 2017年 weiwei-zhang. All rights reserved.
+//
+
+#import "TestView.h"
+
+@implementation TestView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
